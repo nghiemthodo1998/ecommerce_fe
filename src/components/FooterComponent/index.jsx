28604c334d4e780b07ae61417002a3e4626ec329
nghiemthodo1998 <nghiemthodo1998@gmail.com>
2023-02-12 18:59:31 +0700

    header and footer layout

--- src/components/FooterComponent/index.jsx
+++ src/components/FooterComponent/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <footer>
+      <Container fluid>
+        <Row className="mt-5">
+          <Col className="bg-dark text-white text-center py-5">
+            Ecommerce Shop Footer
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default FooterComponent;
--- src/components/HeaderComponent/index.jsx
+++ src/components/HeaderComponent/index.jsx
@@ -1,7 +1,81 @@
 import React from "react";
-
+import {
+  Badge,
+  Button,
+  Dropdown,
+  DropdownButton,
+  Form,
+  InputGroup,
+} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 const HeaderComponent = () => {
-  return <div>HeaderComponent</div>;
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>ECOMMERCE SHOP</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <InputGroup>
+              <DropdownButton id="dropdown-basic-button" title="All">
+                <Dropdown.Item>Electronics</Dropdown.Item>
+                <Dropdown.Item>Cars</Dropdown.Item>
+                <Dropdown.Item>Books</Dropdown.Item>
+              </DropdownButton>
+              <Form.Control type="text" placeholder="Search..." />
+              <Button variant="warning">
+                <i className="bi bi-search"></i>
+              </Button>
+            </InputGroup>
+          </Nav>
+          <Nav>
+            <LinkContainer to="/admin/orders">
+              <Nav.Link>
+                Admin
+                <span className="position-absolute top-1 start-10 translate-middle p-2 bg-danger border border-light rounded-circle"></span>
+              </Nav.Link>
+            </LinkContainer>
+            <Nav.Link>Pricing</Nav.Link>
+            <NavDropdown title="Do Nghiem" id="collasible-nav-dropdown">
+              <NavDropdown.Item
+                eventKey="/user/my-orders"
+                as={Link}
+                to="/user/my-orders"
+              >
+                My orders
+              </NavDropdown.Item>
+              <NavDropdown.Item eventKey="/user" as={Link} to="/user">
+                My profile
+              </NavDropdown.Item>
+              <NavDropdown.Item>Logout</NavDropdown.Item>
+            </NavDropdown>
+            <LinkContainer to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/register">
+              <Nav.Link>Register</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <Badge pill bg="danger">
+                  2
+                </Badge>
+                <i className="bi bi-cart-dash"></i>
+                <span className="ms-1">CART</span>
+              </Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default HeaderComponent;
