616eb30a606ac7b27ec877086ce47c24e22688b4
nghiemthodo1998 <nghiemthodo1998@gmail.com>
2023-02-14 23:22:10 +0700

    update category and carousel layout

---
+++ src/components/CategoryCardComponent/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { LinkContainer } from "react-router-bootstrap";
+
+const CategoryCardComponent = ({ category, index }) => {
+  const images = [
+    "https://cdn.tgdd.vn/Files/2019/07/25/1181734/do-sau-truong-anh-la-gi-cach-thiet-lap-de-chup-anh-dep-nhat--1.jpg",
+    "https://cdn.tgdd.vn/Files/2019/07/25/1181734/do-sau-truong-anh-la-gi-cach-thiet-lap-de-chup-anh-dep-nhat--1.jpg",
+    "https://cdn.tgdd.vn/Files/2019/07/25/1181734/do-sau-truong-anh-la-gi-cach-thiet-lap-de-chup-anh-dep-nhat--1.jpg",
+    "https://cdn.tgdd.vn/Files/2019/07/25/1181734/do-sau-truong-anh-la-gi-cach-thiet-lap-de-chup-anh-dep-nhat--1.jpg",
+  ];
+  return (
+    <Card>
+      <Card.Img variant="top" src={images[index]} />
+      <Card.Body>
+        <Card.Title>{category}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <LinkContainer to="/product-list">
+          <Button variant="primary">Go to Category</Button>
+        </LinkContainer>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CategoryCardComponent;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import CarouselComponent from "../../components/CarouselComponent";
+import CategoryCardComponent from "../../components/CategoryCardComponent/index";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const categories = ["Book", "Laptop", "Camera", "Mobile"];
+  return (
+    <>
+      <CarouselComponent />
+      <Container>
+        <Row xs={1} md={2} className="g-4 mt-5">
+          {categories.map((category, index) => (
+            <CategoryCardComponent
+              key={index}
+              category={category}
+              index={index}
+            />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default HomePage;
